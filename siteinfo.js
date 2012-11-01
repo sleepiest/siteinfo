ebdc6538769551dc18ce91d899f680855752515e
sleepiest <sleepiest@gmail.com>
2012-11-01 16:20:18 +0900

    add siteinfo.js
    siteinfo of chikyu-no-cocolo.

---
+++ siteinfo.js
@@ -0,0 +1,26 @@
+var SITEINFO = [
+    /* sample
+    {
+        url:          'http://(.*).google.+/(search).+',
+        nextLink:     'id("navbar")//td[last()]/a',
+        pageElement:  '//div[@id="res"]/div',
+        exampleUrl:   'http://www.google.com/search?q=nsIObserver',
+    },
+    */
+    /* template
+    {
+        url:          '',
+        nextLink:     '',
+        pageElement:  '',
+        exampleUrl:   '',
+    },
+    */
+
+    {
+        url:          '^http://chikyu-no-cocolo\\.cocolog-nifty\\.com/blog/\\d+/\\d+/post-.*\\.html',
+        nextLink:     'id("alpha-inner")//a[text()="次のページ"]',
+        pageElement:  '//*[@class="entry-asset asset hentry"]',
+        exampleUrl:   'http://chikyu-no-cocolo.cocolog-nifty.com/blog/2012/10/post-1654.html',
+    },
+
+]
